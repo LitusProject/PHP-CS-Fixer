ad0f8d5c7e78f08e8ada560bffa2c24ce1cf79f0
Bram Gotink <bram@gotink.me>
2014-02-26 16:34:35 +0100

    updated git-hooks script

--- Resources/git-hooks/pre-commit.sh
+++ Resources/git-hooks/pre-commit.sh
@@ -31,8 +31,8 @@ if git diff --cached --quiet --exit-code; then
     exit 0
 fi
 
-git diff --cached --name-only --diff-filter=A -z $against |
-    xargs vendor/bin/php-cs-fixer fix |
+git diff --cached --name-only --diff-filter=ACMR -z $against |
+    xargs -0 vendor/bin/php-cs-fixer fix |
     splitgrep -E -e '^\s+[0-9]+\)' |
     cut -d')' -f2 | tr -d ' ' |
     xargs git add
