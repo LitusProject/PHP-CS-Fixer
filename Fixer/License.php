444217b72b0147d4fd6101cedac570323d2048f9
Bram Gotink <bram@gotink.me>
2014-03-31 15:20:57 +0200

    added license to composer.json, changed GPL v3 to GPL v3+

--- Fixer/License.php
+++ Fixer/License.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Litus/CodeStyle creates some helpers for php-cs-fixer project by Fabien Potencier.
- * Built with all the love in the world by @bgotink, licensed under the GPL v3.
+ * Built with all the love in the world by @bgotink, licensed under the GPL v3+.
  *
  * @author Bram Gotink <bram.gotink@litus.cc>
  *
- * @license GPL v3 <https://gnu.org/licenses/gpl.html>
+ * @license GPL v3+ <https://gnu.org/licenses/gpl.html>
  */
 
 namespace Litus\CodeStyle\Fixer;
